ab7582f29638f53bb80f5fc9cb2cad00419750b5
ninadbhamburdekar <>
2024-07-20 18:52:20 -0700

    enrich thehindu use openai for 1d summarization

---
+++ rss_index_enrich1_thehindu.py
@@ -0,0 +1,125 @@
+from datetime import datetime
+from datetime import timezone
+import pytz
+import rss_newsoutlets
+import time
+from elasticsearch import Elasticsearch
+from argparse import ArgumentParser
+import ssl
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+import feedparser
+import env
+import yaml
+import json
+from openai import OpenAI
+
+
+
+ESNODES = env.ESNODES
+esclient = env.setup_esclient(ESNODES)
+
+### Read Polling Config
+with open("rsscollect_config.yaml") as stream:
+    try:
+        yamlconfig = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+FEEDINDEX = yamlconfig['elasticconfig']['feedindex']
+
+
+def main():
+
+    ## Parse input arguments
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--feed", dest="feedconfig", default="ALL",
+                        help="Enrich a specific feed config")
+    parser.add_argument("-l", "--lookback", dest="lookback", default="96",
+                        help="Override lookback time (default is 96hrs)")
+    args = parser.parse_args()
+
+    if args.feedconfig != "ALL":
+        print("Running feed config: {}".format(args.feedconfig))
+        try:
+            enrich_feed_docs(args.feedconfig,
+                         FEEDINDEX, esclient=esclient)
+        except Exception as e:
+            print("Exception processing feed config: %s" %(e))
+
+    else:
+        for config in yamlconfig['outlet_rss_configs']:
+            if 'thehindu' in config:
+                print("Processing config: {}".format(config))
+                stat = enrich_feed_docs(config,
+                             FEEDINDEX, esclient=esclient)
+                print(stat)
+
+    print("Done!")
+
+def enrich_feed_docs(feed_id, feed_index, esclient):
+
+        #query = {"match_phrase": {"source_outlet": "TheHindu"}}
+        query = {"bool":
+                     {
+                         "must": [
+                             {"range": {"eventtime": {"gte": "now-365d", "lt": "now"}}},
+                             {"match": {"source": feed_id}}
+                            ],
+                         "must_not": [
+                             {"exists": {"field": "post_process.enrich1"}}
+                            ]
+                     }
+                }
+
+
+        feed_docs = []
+
+        feed_docs = esclient.search(index=feed_index, query=query, size=10000)
+
+        print("Got %s results: " % len(feed_docs.raw['hits']['hits']))
+
+        for result in feed_docs.raw['hits']['hits']:
+            print("Processing %s" %(result))
+            try:
+
+                htmlcontent = get_url_content(result['_source']['data']['links'][0]['href'])
+                text  = extract_text_content(htmlcontent, re.search('(\\d+$)', result['_source']['document_id'])[0])
+                text = text.replace(u'\xa0', u' ')
+
+                if 'post_process' not in result['_source'].keys():
+                    result['_source']['post_process'] = {}
+
+                if 'enrich1' not in result['_source']['post_process'].keys():
+                    result['_source']['post_process']['enrich1'] = datetime.now()
+
+                if 'content' not in result['_source'].keys():
+                    result['_source']['content'] = {}
+                    result['_source']['content']['enrich1'] = text
+
+                esclient.update(index=feed_index, id=result['_id'], doc=result['_source'])
+
+            except Exception as e:
+                print("Exception processing ID: %s URL: %s - %s" %(result['_id'], result['_source']['data']['links'][0]['href'], e))
+
+def get_url_content(url):
+    http_headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+    }
+    req = urllib.request.Request(url, headers=http_headers)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+    return html
+
+def extract_text_content(htmlcontent, identifier):
+    soup = BeautifulSoup(htmlcontent, 'html.parser')
+    content = ""
+    for res in soup.find_all("div", id="content-body-" + identifier)[0].find_all("p")[0:-1]:
+        content = content + " " + res.get_text()
+
+    return content.strip()
+
+# __name__
+if __name__ == "__main__":
+    main()
---
+++ rss_index_order2_openai.py
@@ -0,0 +1,89 @@
+from datetime import datetime
+from datetime import timezone
+import pytz
+import rss_newsoutlets
+import time
+from elasticsearch import Elasticsearch
+from argparse import ArgumentParser
+import ssl
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+import feedparser
+import env
+import yaml
+import json
+from openai import OpenAI
+
+
+ESNODES = env.ESNODES
+esclient = env.setup_esclient(ESNODES)
+
+### Read Polling Config
+with open("rsscollect_config.yaml") as stream:
+    try:
+        yamlconfig = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+FEEDINDEX = yamlconfig['elasticconfig']['feedindex']
+
+openaiclient = OpenAI(
+    # This is the default and can be omitted
+    api_key=env.openai_api_token,
+)
+
+def main():
+
+    ## Parse input arguments
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--feed", dest="feedconfig", default="ALL",
+                        help="Enrich a specific collection")
+    parser.add_argument("-l", "--lookback", dest="lookback", default="24",
+                        help="Override lookback time (default is 24hrs)")
+    args = parser.parse_args()
+
+    weekly = weekly_summary(openaiclient, esclient)
+
+    print("Done!")
+
+def weekly_summary(openaiclient, esclient):
+
+    summaries = []
+    query = {"bool": {"must": [{"range": {"eventtime": {"gte": "now-1d", "lt": "now"}}}, {"match": {"process_order": 0}}]}}
+
+    esdata = esclient.search(index="estest-index1", query=query, size=5000)
+
+    print("Processing total items: %s" %(esdata['hits']['total']['value']))
+
+    for newsitem in esdata['hits']['hits']:
+        timestamp = datetime.fromisoformat(newsitem['_source']['eventtime'])
+        summaries.append("At "+str(timestamp)+": "+newsitem['_source']['data']['summary'])
+
+    msg = {"role": "user", "content": "SAMPLE CONTENT"}
+    msg['content'] = ("summarize top five themes including historical context across the following headlines, a high level timeline and highlight any cybersecurity implications or predictions based on input events. Output in json under keys: "
+                      + "{'topics': {'topic_category': [highlights]}, 'timeline', 'cybersecurity_implications']}" + " : ") + str(summaries)
+
+    response1 = openaiclient.chat.completions.create(messages=[msg], model="gpt-4o", max_tokens=4000, temperature=0.4)
+
+    data = {}
+    data['openaisummaryjson'] = json.loads(response1.choices[0].message.content.replace('```json', '').replace('```', '').strip())
+    data['metadata'] = {}
+    data['metadata']['enrich2'] = {}
+    data['metadata']['enrich2']['timestamp'] = datetime.now(timezone.utc)
+    data['metadata']['enrich2']['itemcount'] = len(summaries)
+    data['metadata']['enrich2']['inputlength'] = len(msg['content'])
+    data['metadata']['enrich2']['outputlength'] = len(json.dumps(data['openaisummaryjson']))
+    data['metadata']['enrich2']['openaiusage'] = str(response1.usage)
+    data['process_order'] = 2
+
+    data ['eventtime'] = datetime.now(timezone.utc)
+
+    storesummary = esclient.index(index="estest-index1", id="dailysummary_" + str(datetime.now(timezone.utc)), document=data)
+    print(data['openaisummaryjson'])
+
+
+# __name__
+if __name__ == "__main__":
+    main()
+
